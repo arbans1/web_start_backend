6dc06fbb98f42da845b9b1186c0a00daf0594882
arbans1 <70884827+arbans1@users.noreply.github.com>
2022-12-18 18:40:57 +0900

    FBV로 블로그 포스트 목록 페이지 만들기
    FBV(Function Base View)로 블로그 포스트 목록 페이지 만들기

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    """
+    index 페이지를 보여줍니다.
+    """
+
+    # Post 모델에서 모든 Post 객체를 가져옵니다.
+    posts = Post.objects.all().order_by('-pk')
+
+    # return render(request=request, template_name="blog/index.html")
+
+    # render 함수의 두 번째 인수로 context를 전달합니다.
+    return render(request=request, template_name="blog/index.html", context={"posts": posts})
--- src/web_start/urls.py
+++ src/web_start/urls.py
@@ -1,21 +1,25 @@
-"""web_start URL Configuration
+"""Web_Start URL 구성
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+`urlpatterns '목록은 URL을 뷰로 라우팅합니다.자세한 내용은 다음을 참조하십시오.
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+예 :
+기능보기
+    1. 가져 오기 추가 : my_app 가져 오기 뷰에서
+    2. urlpatterns에 URL을 추가하십시오 : Path ( '', views.home, name = 'home')
+수업 기반 견해
+    1. 가져 오기 추가 : Other_App.Views home에서 가져 오기
+    2. urlpatterns에 URL을 추가하십시오 : Path ( '', home.as_view (), name = 'home')
+다른 urlconf를 포함합니다
+    1. 포함 () 함수 가져 오기 : django.urls import 포함, 경로
+    2. urlpatterns에 URL 추가 : Path ( 'blog/', 포함 ( 'blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(route='admin/', view=admin.site.urls),
+
+    # 사용자 설정
+    path(route='blog', view=include('blog.urls')),
 ]
